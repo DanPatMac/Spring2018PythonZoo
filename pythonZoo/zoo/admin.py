b35fce013fcbf16f9a4b3ea508bd9178de91c3f2
Daniel McNamara <36416478+DanPatMac@users.noreply.github.com>
2018-04-26 14:38:58 +0000

    Added more to models file

--- pythonZoo/zoo/admin.py
+++ pythonZoo/zoo/admin.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Zoo, Exhibit, Neighbors
+
 # Register your models here.
+
+class ZooAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+admin.site.register(Zoo, ZooAdmin)
+
+class ExhibitAdmin(admin.ModelAdmin):
+    list_display = ("id", "name") 
+
+admin.site.register(Exhibit, ExhibitAdmin)   
+
+class NeighborsAdmin(admin.ModelAdmin):
+    list_display = ("id", "toExhibit")
+
+admin.site.register(Neighbors, NeighborsAdmin)
--- pythonZoo/zoo/models.py
+++ pythonZoo/zoo/models.py
@@ -4,3 +4,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Zoo(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter Zoo Name")
+    
+    def __str__(self):
+        return self.name
+class Exhibit(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter Exhibit Name")
+    zoo = models.ForeignKey("Zoo", on_delete=models.SET_NULL, null=True)
+    hasExit = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+class Neighbors(models.Model):
+    fromExhibit = models.ForeignKey("Exhibit", on_delete=models.SET_NULL, null=True, related_name='fromExhibit')
+    direction = models.CharField(max_length=2, help_text="Enter Direction To Travel")
+    toExhibit = models.ForeignKey("Exhibit", on_delete=models.SET_NULL, null=True, related_name='toExhibit')
+
+    def __str__(self):
+        return self.direction
